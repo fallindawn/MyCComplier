647bc3176b60034ab6626f506cae6abcf79ce23c
fallindawn <145995944+fallindawn@users.noreply.github.com>
2025-12-20 14:40:35 +0800

    错误用例

---
+++ test/test_error1.c
@@ -0,0 +1,5 @@
+int main(){
+    int a;
+    a = "hello";  // 错误：将字符串赋值给整数类型
+    return 0;
+}
---
+++ test/test_error2.c
@@ -0,0 +1,6 @@
+int main(){
+    int a;
+    float a;  // 错误：重复声明变量a
+    b=10;    // 错误：变量b未声明
+    return 0;
+}
